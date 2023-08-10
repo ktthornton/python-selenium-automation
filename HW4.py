91ac3773b96eff8ccb4b8e2e3e23d077fc824123
ktthornton <ktknudten@gmail.com>
2023-08-09 20:34:39 -0500

    Initial commit for HW 4 .py and .feature files, assignment complete

---
+++ HW4.py
@@ -0,0 +1,7 @@
+#scenario 3/HW problem 4- Kata exercise
+random_number = 1234567890
+
+for i in random_number:
+    reverse_number = []
+    reverse_number.append(i)
+    print(reverse_number[::-1])
---
+++ features/tests/HW4.feature
@@ -0,0 +1,6 @@
+# Created by ktknu at 8/9/2023
+Feature: # Enter feature name here
+  # Enter feature description here
+
+  Scenario: # Enter scenario name here
+    # Enter steps here
