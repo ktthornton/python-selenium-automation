570e5d413858ea932525d1bc18e614be15873b60
ktthornton <ktknudten@gmail.com>
2023-08-11 16:54:30 -0500

    Updated HW4.py file - complete

--- HW4.py
+++ HW4.py
@@ -1,7 +1,15 @@
 #scenario 3/HW problem 4- Kata exercise
 random_number = 1234567890
 
-for i in random_number:
+
+def reverse_number_function(num):
+    num = str(num)
     reverse_number = []
-    reverse_number.append(i)
-    print(reverse_number[::-1])
+    for i in num:
+        num = int(num)
+        reverse_number.append(i)
+        reverse_sort = reverse_number[::-1]
+    return reverse_sort
+
+
+print(reverse_number_function(random_number))
